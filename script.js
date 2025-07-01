775c2c3f8af4f22b52403b5c9688051cf4bfcf3d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-01 14:32:32 +0200

    [add] variables, console.log

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // SVOLGIMENTO ESERCIZIO
 
+
+let priceKm = 0.21;
+let passengerAge = parseInt(prompt('Inserisci la tua età'));
+let passengerKm = parseInt(prompt('Inserisci i KM che vuoi percorrere'));
+let cost = priceKm * passengerKm;
+let finalCost;
+
+console.log('Il costo del tuo viaggio è di €', finalCost);
